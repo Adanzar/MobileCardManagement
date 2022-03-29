b6ce2c26a3a2475b4bfd16ca4817f022807e7610
francisco.martinez <francisco.martinez@worksut.com>
2022-03-29 09:53:00 +0200

    Create the project CardHolder

---
+++ app/src/main/java/com/devanddes/cardholder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.devanddes.cardholder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
